8e1d25e6c833d8ec410a73d16cad5bc50a5b3c58
gitcuber369 <arpitchaudhary2003@gmail.com>
2025-03-04 13:25:37 +0530

    [update]:app logo changed

--- app/(tabs)/Home/AdminHome.tsx
+++ app/(tabs)/Home/AdminHome.tsx
@@ -376,7 +376,7 @@ const Header = () => {
       ]}
     >
       <Animated.Image
-        source={require("@/assets/images/icon/icon.png")}
+        source={require("@/assets/images/icon/i con.png")}
         style={[{ width: 64, height: 64 }, iconAnimatedStyle]}
         resizeMode="cover"
       />
